e436550c9a54ade635077b9a348a361f0ac7a226
Ruben Romero Montes <rromerom@redhat.com>
2018-03-05 13:16:36 +0100

    add es-client app

---
+++ es-client/models/connection.js
@@ -0,0 +1,30 @@
+const elasticsearch = require('elasticsearch');
+
+function initializeClient() {
+  if(process.env.ES_SECURE !== undefined && process.env.ES_SECURE) {
+    return new elasticsearch.Client( {
+      host: [
+        {
+          host: process.env.ES_SVC,
+          auth: process.env.ES_USER + ':' + process.env.ES_PASS,
+          protocol: 'https',
+          port: 9200
+        }
+      ]
+    });
+  } else {
+    return new elasticsearch.Client( {
+      host: [
+        {
+          host: process.env.ES_SVC,
+          protocol: 'http',
+          port: 9200
+        }
+      ]
+    });
+  }
+}
+
+const client = initializeClient();
+
+module.exports = client;
